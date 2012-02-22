a95430a5f5e9d77d544c20ba4e895f6dfbfb40de
Adrian Seyboldt <adrian_seyboldt@web.de>
2012-02-22 21:55:05 +0100

    format check_heap

--- tests/check_heap.c
+++ tests/check_heap.c
@@ -153,33 +153,32 @@ START_TEST (test_heap_delete_min)
 END_TEST
 
 
-Suite *
-money_suite (void)
+Suite * money_suite (void)
 {
-  Suite *s = suite_create ("heap");
+    Suite *s = suite_create ("heap");
 
-  /* Core test case */
-  TCase *tc_core = tcase_create ("Core");
+    /* Core test case */
+    TCase *tc_core = tcase_create ("Core");
 
-  tcase_add_test (tc_core, test_heap_create);
-  tcase_add_test (tc_core, test_heap_grow);
-  tcase_add_test (tc_core, test_heap_insert);
-  tcase_add_test (tc_core, test_heap_find_min);
-  tcase_add_test (tc_core, test_heap_delete_min);
+    tcase_add_test (tc_core, test_heap_create);
+    tcase_add_test (tc_core, test_heap_grow);
+    tcase_add_test (tc_core, test_heap_insert);
+    tcase_add_test (tc_core, test_heap_find_min);
+    tcase_add_test (tc_core, test_heap_delete_min);
 
-  suite_add_tcase (s, tc_core);
+    suite_add_tcase (s, tc_core);
 
-  return s;
+    return s;
 }
 
 
 int main (void)
 {
-  int number_failed;
-  Suite *s = money_suite ();
-  SRunner *sr = srunner_create (s);
-  srunner_run_all (sr, CK_NORMAL);
-  number_failed = srunner_ntests_failed (sr);
-  srunner_free (sr);
-  return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
+    int number_failed;
+    Suite *s = money_suite ();
+    SRunner *sr = srunner_create (s);
+    srunner_run_all (sr, CK_NORMAL);
+    number_failed = srunner_ntests_failed (sr);
+    srunner_free (sr);
+    return (number_failed == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
